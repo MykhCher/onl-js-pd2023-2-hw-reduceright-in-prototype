3cc82bc3bb152b273211f2329604d84cc9e4953c
MykhCher <micher1240gmail.com>
2024-02-21 12:04:39 +0200

    add reduceRight for prototype

---
+++ source/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+function MyArrayProto() {
+    this.reduceRight = function (callback, accumulator) {
+        let result;
+        if (accumulator) {
+            result = accumulator;
+            for (let i = this.length-1; i >= 0; i--) {
+                result = callback(result, this[i], this, i);
+            }
+        } else {
+            result = this[this.length-1];
+            for (let i = this.length-1; i>=0; i--) {
+                result = callback(result, this[i-1], this, i)
+            }
+        }
+        return result;
+    }
+}
+
+function MyArray(...args) {
+    this.length = 0;
+    for (let i = 0; i < args.length; i++) {
+        this[this.length++] = arguments[i]
+    }
+}
+
+MyArray.prototype = new MyArrayProto();
+
+const numbers = new MyArray(1,2,3,4,5);
+const reducedNumbers = numbers.reduceRight(
+    function (pV, cV) {
+        pV.push(cV);
+        return pV
+    },
+    []
+);
+
+console.log(reducedNumbers);
